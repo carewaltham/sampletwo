8a8dffb5016dafe64c2cd8c34e70572b249c3716
vchandran <vchandran@care.com>
2016-01-25 10:49:57 -0500

    first commit

---
+++ src/com/sauce/runner/TestConfiguration.java
@@ -0,0 +1,15 @@
+package com.sauce.runner;
+
+public class TestConfiguration {
+
+	public final static String ON_DEMAND_URL = "ondemand.saucelabs.com";
+	
+	
+	
+	public final static String USERNAME = "vishwachand";
+	
+	public final static String ACCESS_KEY = "385b5eb2-e166-4144-b93e-f637b9b86bc8";
+	
+	public static String onDemandUrl = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+	
+}
